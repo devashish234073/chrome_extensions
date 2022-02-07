7e46dfe6279f05eae0f8e78a8e489a33c3a79192
Devashish Priyadarshi <devashish234073@gmail.com>
2022-02-07 19:49:53 +0530

    completed youtube view noter

--- youtube_view_noter/script.js
+++ youtube_view_noter/script.js
@@ -1,15 +1,59 @@
 var tabid = document.querySelector("#tabid");
 var pageTitle = document.querySelector("#pageTitle");
 var tabIcon = document.querySelector("#tabIcon");
-
+var mainTable = document.querySelector("#mainTable");
 console.log(chrome.tabs);
+
+
 
 async function getCurrentTab() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
+    pageTitle.style.color = "white";
+    if(tab.url.indexOf("https://www.youtube.com/watch")==0){
+        pageTitle.style.backgroundColor = "green";
+        pageTitle.innerText = "Youtube video playing";
+    } else if(tab.url.indexOf("https://www.youtube.com")==0){
+        pageTitle.style.backgroundColor = "red";
+        pageTitle.innerText = "Youtube site. But no video playing.";
+    } else {
+        pageTitle.style.backgroundColor = "red";
+        pageTitle.innerText = "Not youtube site.";
+    }
     //pageTitle.innerText = JSON.stringify(tab);
     tabIcon.src = tab["favIconUrl"];
     return tab;
+}
+
+function noteView(url,views) {
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    td1.style.fontSize = "10px";
+    td2.style.fontSize = "10px";
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    mainTable.appendChild(tr);
+    
+    td1.innerText = url;
+    var vs = "";
+    for(let i=0;i<views.length;i++) {
+        vs += views[i] + "\r\n";
+    }
+    td2.innerText = vs;
+    /*var obj = {};
+    chrome.storage.sync.get([url], function(result) {
+        if(result!=undefined && result.value!=undefined) {
+            obj[url] = result.value + "\r\n" + views;
+        } else {
+            obj[url] = views;
+        }
+        td1.innerText = url;
+        td2.innerText =  obj[url];
+    });
+    chrome.storage.sync.set(obj, function() {
+        console.log('Value is set to ' + JSON.stringify(obj));
+    });*/
 }
 
 getCurrentTab();
@@ -22,3 +66,15 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         files: ['srciptToRunOnTab.js']
     });
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.cntr!= undefined ) {
+        noteView(sender.tab.url,request["cntr"][sender.tab.url]);
+        sendResponse({farewell: request["cntr"][sender.tab.url]+" received"});
+      } 
+    }
+  );
--- youtube_view_noter/srciptToRunOnTab.js
+++ youtube_view_noter/srciptToRunOnTab.js
@@ -1,48 +1,18 @@
-document.body.style.backgroundColor = "lightgreen";
-//For youtube
-var youtubeTitle = document.querySelectorAll("#video-title");
-if (youtubeTitle != undefined) {
-    for (var i = 0; i < youtubeTitle.length; i++) {
-        if (i % 3 == 0) {
-            youtubeTitle[i].style.backgroundColor = "red";
-        } else if (i % 3 == 1) {
-            youtubeTitle[i].style.backgroundColor = "green";
-        } else if (i % 3 == 2) {
-            youtubeTitle[i].style.backgroundColor = "blue";
-        }
-        youtubeTitle[i].style.padding = "10px";
-        youtubeTitle[i].style.borderRadius = "10px";
-        youtubeTitle[i].style.boxShadow = "0px 0px 18px lightgreen";
+var counter = document.querySelector(".style-scope ytd-video-view-count-renderer span");
+if (counter != undefined) {
+    var data = localStorage.getItem("youtube_view_noter");
+    if (data == null || data == undefined) {
+        data = {};
+    } else {
+       data = JSON.parse(data);
     }
-    var viewsCounter = document.querySelectorAll("span.ytd-video-meta-block");
-    if(viewsCounter!=undefined) {
-        for(var i=0;i<viewsCounter.length;i++) {
-            var cntr = viewsCounter[i];
-            cntr.innerText = "No views | 0 views";
-        }
+    if (data[window.location.href] == undefined) {
+        data[window.location.href] = ["[" + (new Date()) + ": " + counter.innerText + "]"];
+    } else {
+        data[window.location.href].push("[" + (new Date()) + ": " + counter.innerText + "]");
     }
-}
-//For Google Searchbox
-var googleInpt = document.querySelector("input[type='text']");
-if (googleInpt != undefined && googleInpt.getAttribute("aria-label") == "Search") {
-    if (googleInpt.getAttribute("jsaction") != undefined) {
-        if (googleInpt.getAttribute("jsaction").startsWith("paste")) {
-            googleInpt.style.backgroundColor = "red";
-            googleInpt.style.color = "white";
-            googleInpt.style.fontSize = "20px";
-            googleInpt.value = "I will not search anything today";
-        }
-    }
-}
-//For Google Search button
-var googleSearchBtn = document.querySelector("input[value='Google Search']");
-if (googleSearchBtn != undefined) {
-    googleSearchBtn.value = "Mera "+parseInt(Math.random()*100)+" Search";
-    googleSearchBtn.style.height = "40px";
-    googleSearchBtn.style.fontSize = "18px";
-}
-//For stackoverflow
-var questionHyperLnk = document.querySelector(".question-hyperlink");
-if(questionHyperLnk!=undefined){
-    questionHyperLnk.innerText = "Why is earth so round?";
+    localStorage.setItem("youtube_view_noter", JSON.stringify(data));
+    chrome.runtime.sendMessage({ cntr: data }, function (response) {
+        console.log(response.farewell);
+    });
 }
