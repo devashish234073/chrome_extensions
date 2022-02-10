619fcf017d6def5dbe64de205215a274e34380c4
Devashish Priyadarshi <devashish234073@gmail.com>
2022-02-10 14:46:25 +0530

    added code to nadle NaN view count comparison

--- youtube_view_noter/srciptToRunOnTab.js
+++ youtube_view_noter/srciptToRunOnTab.js
@@ -14,6 +14,9 @@ if (counter != undefined) {
     } else {
         var lastElem = data[window.location.href][data[window.location.href].length - 1];
         var lastElemCount = parseInt(String(lastElem.split("):")[1]).split(",").join(""));
+        if(isNaN(lastElemCount)) {
+            lastElemCount = 0;
+        }
         //console.log(`Analyzing view count: ${counter.innerText}`);
         if (lastElem.indexOf(counter.innerText + "]") == -1) {//if the viewcount is not noted
             var currentElemCount = parseInt(String(counter.innerText).split(",").join(""));
